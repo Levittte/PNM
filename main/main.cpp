5612a20a58b671c61b4e74b936bfa7cea04dd677
dzuba <chepik29rus@gmail.com>
2019-02-24 22:32:02 +0300

    rect + trapez methods

--- main/main.cpp
+++ main/main.cpp
@@ -7,18 +7,25 @@
 #include "function.h"
 #include "methods.h"
 
-int main(int argc, char* argv[]){
+int main(){
 
-//   if (argc > 1){
-//     std::istringstream ss(argv[1]);
-//     size_t iters;
-//     if (!(ss >> iters)){
-//       throw std::invalid_argument("Wrong ARGV");
-//     } else {
-//       N=iters;
-//     }
-//   }   
+    const double a = 0.3;
+    const double b = 0.8;
+    double res = 0;
+    auto startTime = std::chrono::steady_clock::now();
 
-
-  return 0;
+    res = mid_rect(a, b,200);
+    std::cout<<"MID:"<<res<<std::endl;
+    res = left_rect(a, b,200);
+    std::cout<<"LEFT:"<<res<<std::endl;
+    res = right_rect(a, b,200);
+    std::cout<<"RIGHT:"<<res<<std::endl;
+    res = trapez(a, b,200);
+    std::cout<<"TRAPEZ:"<<res<<std::endl;
+    res = simpson(a, b,200);
+    std::cout<<"SIMPSON:"<<res<<std::endl;
+    auto runTime= std::chrono::steady_clock::now(); 
+    auto runtimeDuration = std::chrono::duration_cast<std::chrono::duration<double>>(runTime - startTime);
+    std::cout<<"RUNTIME:"<<runtimeDuration.count()<<std::endl;
+    return 0;
 }
--- main/methods.cpp
+++ main/methods.cpp
@@ -1,1 +1,60 @@
 #include "function.h"
+#include "methods.h"
+
+double mid_rect(const double a, const double b, const size_t n){
+    double result = 0;
+    const double h = (b-a)/n; 
+
+    #pragma omp parallel for reduction (+:result)
+    for(size_t i=0; i < n; i++){
+
+        result += f(a + h * (i + 0.5)); 
+    }
+
+    return result*h;
+}
+
+double left_rect(const double a, const double b, const size_t n){
+    double result = 0;
+    const double h = (b-a)/n; 
+
+    #pragma omp parallel for reduction (+:result)
+    for(size_t i=0; i < n; i++){
+
+        result += f(a + h * (i-1)); 
+    }
+
+    return result*h;
+}
+
+double right_rect(const double a, const double b, const size_t n){
+    double result = 0;
+    const double h = (b-a)/n; 
+
+    #pragma omp parallel for reduction (+:result)
+    for(size_t i=0; i < n; i++){
+
+        result += f(a + h * i); 
+    }
+
+    return result*h;
+}
+
+double simpson(const double a, const double b, const size_t n){
+
+}
+
+double trapez(const double a, const double b, const size_t n){
+    double result = 0;
+    const double h = (b - a) / n;
+
+    result +=f(a);
+    result +=f(b);
+
+    #pragma omp parallel for reduction (+:result)
+    for(size_t i = 0; i < n; i++) {
+        result+=f(a+i*h);
+    }
+
+    return result*h;
+}
